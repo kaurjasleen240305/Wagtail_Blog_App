7e7b42288f9e3dfaadb21ca721bf67d1fcda852e
kaurjasleen240305 <jasleen_k@ee.iitr.ac.in>
2023-12-05 17:52:57 +0530

    Rich Text field learnt

--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,10 @@
-from django.db import models
-
-# Add these:
 from wagtail.models import Page
 from wagtail.fields import RichTextField
 from wagtail.admin.panels import FieldPanel
+
+from wagtail.search import index
+from django.db import models
+
 
 
 class BlogIndexPage(Page):
@@ -13,4 +14,18 @@ class BlogIndexPage(Page):
         FieldPanel('intro')
     ]
 
-# Create your models here.
+class BlogPage(Page):
+    date = models.DateField("Post date")
+    intro = models.CharField(max_length=250)
+    body = RichTextField(blank=True)
+
+    search_fields = Page.search_fields + [
+        index.SearchField('intro'),
+        index.SearchField('body'),
+    ]
+
+    content_panels = Page.content_panels + [
+        FieldPanel('date'),
+        FieldPanel('intro'),
+        FieldPanel('body'),
+    ]
